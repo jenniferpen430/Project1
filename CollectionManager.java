2a2bfbcb39a3ddceeb939db82bc37b02b7c2f011
Jennifer Penaranda <jennifer.pen.x@gmail.com>
2021-09-14 03:14:55 -0400

    adding run method

--- CollectionManager.java
+++ CollectionManager.java
@@ -1,3 +1,21 @@
-public void run() {
+import java.util.Scanner;
+
+
+/**
+ The CollectionManager class is used to run
+
+ @author Adams, Jennifer
+ */
+public class CollectionManager {
+    private Collection collection;
+
+    public void run() {
+        System.out.println("Collection Manager starts running.");
+        Scanner scanner = new Scanner(System.in);
+        String line  = scanner.nextLine();
+        collection = new Collection();
+        while(!line.equals('Q')){
 
         }
+    }
+}
