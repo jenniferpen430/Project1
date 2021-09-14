ce36f4b1e202e9e4eb346b142477f1572c37ebb6
Jennifer Penaranda <jennifer.pen.x@gmail.com>
2021-09-14 03:05:31 -0400

    adding runproject1 class

---
+++ RunProject1.java
@@ -0,0 +1,17 @@
+
+/**
+ The RunProject1 class is used to run the run() method in the CollectionManager class and efficiently
+ perform overall functions for the CollectionManager class.
+ @author Adams, Jennifer
+ */
+public class RunProject1 {
+
+    public static void main(String[] args) {
+
+        new Kiosk().run();
+
+
+
+    }
+
+}
