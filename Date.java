6ada5e3fc57fa475e7cb4ef589e7e2443b1d2ec4
Jennifer Penaranda <jennifer.pen.x@gmail.com>
2021-09-17 02:04:11 -0400

    adding date method

--- Date.java
+++ Date.java
@@ -1,8 +1,21 @@
+package albums;
+
+import java.util.Calendar;
+import java.util.StringTokenizer;
+
 public class Date implements Comparable<Date> {
     private int year;
     private int month;
     private int day;
-    public Date(String date) {} //take “mm/dd/yyyy” and create a Date object
+
+    //take “mm/dd/yyyy” and create a Date object
+    public Date(String date) {
+        StringTokenizer st = new StringTokenizer(date, "/");
+        month = Integer.parseInt(st.nextToken());
+        day = Integer.parseInt(st.nextToken());
+        year = Integer.parseInt(st.nextToken());
+    }
+
     public Date() {} //create an object with today’s date (see Calendar class)
 
     public boolean isValid() {}
@@ -11,4 +24,6 @@ public class Date implements Comparable<Date> {
     public int compareTo(Date date) {
 
     }
+
+
 }
