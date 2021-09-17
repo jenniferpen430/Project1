790e4a50e9d4a751ec2d6569fb6aef892c704aa7
Jennifer Penaranda <jennifer.pen.x@gmail.com>
2021-09-17 02:28:11 -0400

    adding getter and equals methods

--- Album.java
+++ Album.java
@@ -5,9 +5,51 @@ public class Album {
     private Data releaseDate;
     private boolean isAvailable;
 
-    @Override
-    public boolean equals(Object obj) { }
+    //
+    public Album(String name, String date) {
+        title = "";
+        this.artist = artist;
+        checkOut = false;
+        releaseDate = new Date(date);
+    }
 
+    //
+    public String getTitle() {
+        return title;
+    }
+
+    //
+    public String getArtist() {
+        return artist;
+    }
+
+    //
+    public Genre getGenre() {
+        return genre;
+    }
+
+    //
+    public boolean getIsAvailable() {
+        return isAvailable;
+    }
+
+    //
     @Override
-    public String toString() { }
+    public boolean equals(Object obj) {
+        if(obj instanceof Album){
+            Album album = (Album) obj;
+            return album.title.equals(this.title) && album.artist.equal(this.artist);
+        }
+    }
+
+    //
+    @Override
+    public String toString() {
+        if(isAvailable){
+            return
+        }
+        else{
+            return
+        }
+    }
 }
